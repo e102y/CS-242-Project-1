fffef3eb8e46f86556c6cad7a7056495e9d558ca
e102y <rhodesou@sonoma.edu>
2016-11-01 19:30:46 -0700

    actual file

---
+++ bgen.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+//#include <cstdlib>
+//#include "OrderedSet.hpp"
+using namespace std;
+
+void Generate (int T, float P, int G);
+void Statistics (int T, float P, int G);
+int main (string type, int N, int T, float P, int G, string filename )
+{
+	if(P>1 || (type != "gen" && type != "stat") || G>3 || G<0){
+
+	ostream << ("one of the inputs was wrong, please try again.");
+
+	}	
+
+
+srand(0);
+
+
+
+	
+	return 0;
+}
